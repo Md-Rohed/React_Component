10870eacbccf7dc3bf268784e4a0991c3c292d5e
Md-Rohed <mdrohed85@gmail.com>
2022-10-02 22:31:39 +0600

    Component Controlled

---
+++ src/component/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export default class SearchBar extends Component {
+  state = { input: "" };
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form style={{ border: "1px solid ", padding: "10px", margin: "10px" }}>
+          <label> Search For Image:</label>
+          <br />
+          <input
+            type="text"
+            value={this.state.input}
+            onChange={this.handleChange}
+            style={{ width: "100%", padding: "10px" }}
+          />
+          <p>{this.state.input}</p>
+        </form>
+      </div>
+    );
+  }
+}
