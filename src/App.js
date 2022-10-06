9e2d90c1bddd5ab65e60c7e0be983aeeb34bd32f
Md-Rohed <mdrohed85@gmail.com>
2022-10-06 23:59:59 +0600

    render image

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import axios from "axios";
 import React, { Component } from "react";
 
 export default class App extends Component {
-  onSearchSubmit(input) {
-    axios.get("https://api.unsplash.com/search/photos", {
+  state = { image: [] };
+  onSearchSubmit = async (input) => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: input },
       headers: {
         Authorization: "Client-ID MX32KIhRmXMLgpue-cDsexGa88b6AoHg9vfTPobQPOM",
       },
     });
-  }
+    this.setState({ image: response.data.image });
+  };
   render() {
     return (
       <div className="App">
         <SearchBar onSubmit={this.onSearchSubmit} />
+        found {this.images.length} images
       </div>
     );
   }
